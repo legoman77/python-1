ad121a60e46ac736633c988865103ea82663f216
Rees Payne <rplegos@gmail.com>
2015-06-10 20:38:21 -0400

    save

---
+++ utils.py
@@ -0,0 +1,14 @@
+"""This is my utilities file for cool functions."""
+
+import colors as c
+
+def ask(question,color=c.yellow):
+    print(color + question + c.reset)
+    answer = input('> ' + c.base3).lower().strip()
+    print(c.reset)
+    return answer
+
+if __name__ == '__main__':
+    print(c.clear)
+    name = ask('What is your name?')
+    color = ask ('What is your name in color?',c.random_color())
